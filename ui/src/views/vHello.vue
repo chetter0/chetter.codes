3fcedf87a3325f59a211543178776c264616ef78
chetter0 <christie@theetters.com>
2020-05-06 08:53:14 -0400

    basic hello edits
    removed white space from trees image, changed hello message, noscript message

--- ui/src/views/vHello.vue
+++ ui/src/views/vHello.vue
@@ -2,10 +2,8 @@
   <div class="view">
     <div class="hello-bulletin-outer">
       <div class="hello-bulletin-inner">
-        <h1>Content coming soon!</h1>
+        <h1>Welcome!</h1>
       </div>
     </div>
   </div>
 </template>
-<style scoped>
-</style>
